8d1bb27afb510587be7f6263105d7b2d44d863fb
md-hasan-akon1 <akonhasan680@gmail.com>
2024-09-30 17:46:00 +0600

    first commit

---
+++ Helper-javascript/login.js
@@ -0,0 +1,46 @@
+document.getElementById('signinForm').addEventListener('submit', async function (event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    // Create the data object to be sent
+    const formData = {
+        email,
+        password,
+    };
+    console.log(formData)
+
+    try {
+        // Send the data to the backend API
+        const response = await fetch('http://localhost:3000/api/v1/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+
+
+
+        const result = await response.json();
+        if (result.success) {
+            localStorage.setItem('userData', JSON.stringify(result));
+            if (result.data.role == 'HELPER') {
+                window.location.href = 'http://127.0.0.1:5500/Helper/Open.html';
+            }
+            if (result.data.role == 'CUSTOMER') {
+                window.location.href = 'http://127.0.0.1:5500/User/Dashboard.html'
+            }else(window.location.href = 'http://127.0.0.1:5500')
+
+        }
+
+
+        //window.location.href = 'http://127.0.0.1:5500/User/Dashboard.html'; 
+
+    } catch (error) {
+        console.error('Error:', error);
+        alert(error.message);
+    }
+
+});
