86fe521f985b8f4f494afe81c69049338cb3583a
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-10-07 17:27:57 +0600

    helper sign up page dreaft

--- src/app/(auth)/helper-sign-up/page.tsx
+++ src/app/(auth)/helper-sign-up/page.tsx
@@ -5,25 +5,60 @@ import MainIcon from "@/assets/images/main-ico.png";
 import GoogleIcon from "@/assets/images/google.webp";
 
 export default function HelperSignupForm() {
-  const [showBusinessFields, setShowBusinessFields] = useState(false);
-  const [isLicensed, setIsLicensed] = useState(false);
-  const [isInsured, setIsInsured] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    accountType: "individual",
+    businessLegalName: "",
+    businessEIN: "",
+    email: "",
+    phone: "",
+    address: "",
+    apartment: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    serviceLocation: "",
+    isLicensed: false,
+    isInsured: false,
+    password: "",
+    termsAccepted: false,
+    policyAccepted: false,
+    licenseFile: null,
+    insuranceFile: null,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value, type, checked, files } = e.target as HTMLInputElement;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : type === "file" && files ? files[0] : value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
   return (
-    <section className="bg-orange-500 py-16">
+    <section className="bg-orange-500 py-16 border-t border-gray-300">
       <div className="container mx-auto px-6">
-        <div className="bg-white rounded-xl shadow-lg p-8 max-w-2xl mx-auto">
-          <div className="text-center">
-            <Image src={MainIcon} alt="Main Icon" className="w-12 h-12 mx-auto mb-4" />
-            <h2 className="text-3xl font-bold">Become helper!</h2>
-            <p className="mt-2">
-              I already have an account?{" "}
-              <a href="/shared/login" className="text-orange-500 hover:underline">
-                Sign In
-              </a>
-            </p>
-          </div>
-
+        <div className="text-center mb-5">
+          <Image
+            src={MainIcon}
+            alt="Main Icon"
+            className="w-12 h-12 mx-auto mb-4 z-10 bg-white p-2 rounded-full overflow-visible"
+          />
+          <h2 className="text-7xl font-bold text-white ">Become helper!</h2>
+          <p className="mt-2 text-white font-semibold text-xl">
+            I already have an account?{" "}
+            <a href="/shared/login" className="text-black hover:underline">
+              Sign In
+            </a>
+          </p>
+        </div>
+        <div className="bg-white rounded-xl shadow-lg p-8 max-w-5xl mx-auto">
           {/* Google Signup */}
           <div className="mt-6 text-center">
             <button className="flex items-center justify-center w-full bg-white text-gray-700 border rounded-md shadow-sm py-2">
@@ -34,12 +69,15 @@ export default function HelperSignupForm() {
           </div>
 
           {/* Signup Form */}
-          <form className="mt-6 space-y-4">
+          <form className="mt-6 space-y-4" onSubmit={handleSubmit}>
             <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
               <div className="flex-1">
                 <label className="form-label">First Name</label>
                 <input
                   type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="First name"
                   required
@@ -49,6 +87,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">Last Name</label>
                 <input
                   type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="Last name"
                   required
@@ -63,7 +104,8 @@ export default function HelperSignupForm() {
                   id="individualRadio"
                   name="accountType"
                   value="individual"
-                  onChange={() => setShowBusinessFields(false)}
+                  checked={formData.accountType === "individual"}
+                  onChange={handleChange}
                   className="form-radio"
                   required
                 />
@@ -77,7 +119,8 @@ export default function HelperSignupForm() {
                   id="businessRadio"
                   name="accountType"
                   value="business"
-                  onChange={() => setShowBusinessFields(true)}
+                  checked={formData.accountType === "business"}
+                  onChange={handleChange}
                   className="form-radio"
                   required
                 />
@@ -87,12 +130,15 @@ export default function HelperSignupForm() {
               </div>
             </div>
 
-            {showBusinessFields && (
+            {formData.accountType === "business" && (
               <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
                 <div className="flex-1">
                   <label className="form-label">Business Legal Name</label>
                   <input
                     type="text"
+                    name="businessLegalName"
+                    value={formData.businessLegalName}
+                    onChange={handleChange}
                     className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                     placeholder="Business Legal Name"
                   />
@@ -101,6 +147,9 @@ export default function HelperSignupForm() {
                   <label className="form-label">Business EIN/Tax ID Number</label>
                   <input
                     type="text"
+                    name="businessEIN"
+                    value={formData.businessEIN}
+                    onChange={handleChange}
                     className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                     placeholder="Business EIN/Tax ID Number"
                   />
@@ -113,6 +162,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">Email Address</label>
                 <input
                   type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="example@gmail.com"
                   required
@@ -122,6 +174,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">Phone Number</label>
                 <input
                   type="text"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="(123) 456 - 7890"
                   required
@@ -134,6 +189,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">Address</label>
                 <input
                   type="text"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="Street"
                   required
@@ -143,6 +201,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">Apartment</label>
                 <input
                   type="text"
+                  name="apartment"
+                  value={formData.apartment}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="Apt"
                 />
@@ -154,6 +215,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">City</label>
                 <input
                   type="text"
+                  name="city"
+                  value={formData.city}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="City"
                   required
@@ -162,6 +226,9 @@ export default function HelperSignupForm() {
               <div className="flex-1">
                 <label className="form-label">State</label>
                 <select
+                  name="state"
+                  value={formData.state}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   required
                 >
@@ -177,6 +244,9 @@ export default function HelperSignupForm() {
                 <label className="form-label">Zip Code</label>
                 <input
                   type="text"
+                  name="zipCode"
+                  value={formData.zipCode}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   placeholder="Zip Code"
                   required
@@ -185,6 +255,9 @@ export default function HelperSignupForm() {
               <div className="flex-1">
                 <label className="form-label">Service Location</label>
                 <select
+                  name="serviceLocation"
+                  value={formData.serviceLocation}
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                   required
                 >
@@ -199,9 +272,11 @@ export default function HelperSignupForm() {
               <div className="flex items-center">
                 <input
                   type="checkbox"
+                  name="isLicensed"
+                  checked={formData.isLicensed}
+                  onChange={handleChange}
                   className="form-checkbox"
                   id="licensedCheck"
-                  onChange={(e) => setIsLicensed(e.target.checked)}
                 />
                 <label htmlFor="licensedCheck" className="ml-2">
                   Licensed or not?
@@ -210,9 +285,11 @@ export default function HelperSignupForm() {
               <div className="flex items-center">
                 <input
                   type="checkbox"
+                  name="isInsured"
+                  checked={formData.isInsured}
+                  onChange={handleChange}
                   className="form-checkbox"
                   id="insuredCheck"
-                  onChange={(e) => setIsInsured(e.target.checked)}
                 />
                 <label htmlFor="insuredCheck" className="ml-2">
                   Insured or not?
@@ -220,21 +297,25 @@ export default function HelperSignupForm() {
               </div>
             </div>
 
-            {isLicensed && (
+            {formData.isLicensed && (
               <div className="mt-4">
                 <label className="form-label">Upload License</label>
                 <input
                   type="file"
+                  name="licenseFile"
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                 />
               </div>
             )}
 
-            {isInsured && (
+            {formData.isInsured && (
               <div className="mt-4">
                 <label className="form-label">Upload Insurance</label>
                 <input
                   type="file"
+                  name="insuranceFile"
+                  onChange={handleChange}
                   className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                 />
               </div>
@@ -244,6 +325,9 @@ export default function HelperSignupForm() {
               <label className="form-label">Password</label>
               <input
                 type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
                 className="w-full p-3 border rounded-md focus:outline-none focus:ring focus:ring-orange-300"
                 placeholder="Enter your password"
                 required
@@ -251,7 +335,15 @@ export default function HelperSignupForm() {
             </div>
 
             <div className="flex items-center justify-start mt-4 gap-2">
-              <input type="checkbox" id="termsUser" className="form-checkbox" required />
+              <input
+                type="checkbox"
+                name="termsAccepted"
+                checked={formData.termsAccepted}
+                onChange={handleChange}
+                id="termsUser"
+                className="form-checkbox"
+                required
+              />
               <label htmlFor="termsUser">
                 I accept the{" "}
                 <a href="#" className="text-orange-500 hover:underline">
@@ -261,7 +353,15 @@ export default function HelperSignupForm() {
             </div>
 
             <div className="flex items-center justify-start mt-2 gap-2">
-              <input type="checkbox" id="policyUser" className="form-checkbox" required />
+              <input
+                type="checkbox"
+                name="policyAccepted"
+                checked={formData.policyAccepted}
+                onChange={handleChange}
+                id="policyUser"
+                className="form-checkbox"
+                required
+              />
               <label htmlFor="policyUser">
                 I have reviewed and agree to Hows{" "}
                 <a href="#" className="text-orange-500 hover:underline">
