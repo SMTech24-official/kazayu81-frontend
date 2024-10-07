c8ae7bf8c57964f33018359315b0094c67677562
Anar <ai.anarul4232@gmail.com>
2024-10-07 17:39:36 +0600

    call to action component modify

--- src/app/(auth)/user-sign-up/page.tsx
+++ src/app/(auth)/user-sign-up/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import mainIcon from "@/assets/images/main-ico.png";
 import ContinueWithGoogoe from "@/components/button/ContinueWithGoogoe";
+import CallToAction from "@/components/home/CallToAction";
 
 const UserSignup = () => {
   return (
@@ -147,28 +148,7 @@ const UserSignup = () => {
       </section>
 
       {/* Content Section */}
-      <div className=" p-5 xl:p-12">
-        <div className="bg-orange-500 py-16 text-center mt-32 rounded-xl">
-          <div className="container mx-auto">
-            <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-white">
-              Need help at your home.
-              <span className="block">Helper on the way</span>
-            </h1>
-            <div className="flex flex-col sm:flex-row gap-3 justify-center">
-              <Link href="/Helper/Helper-signup">
-                <button className="bg-white text-orange-500 py-2 px-4 rounded shadow">
-                  Become helper <i className="fa-solid fa-arrow-right ml-2"></i>
-                </button>
-              </Link>
-              <Link href="/User/User-signup">
-                <button className="bg-transparent border border-white text-white py-2 px-4 rounded shadow">
-                  Find helper
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CallToAction />
     </div>
   );
 };
