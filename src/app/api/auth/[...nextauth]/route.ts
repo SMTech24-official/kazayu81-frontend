33a69953ad56013fdd3bab8a600cf0c4b8c870fd
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-10-08 15:15:28 +0600

    build error solved

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,16 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      //   redirect user to the home page after signing in
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-// Correct the handler spelling and export both GET and POST handlers
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
