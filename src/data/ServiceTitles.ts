8a42ee59d3135eec33c892f2e3ff9fd18c95fd8b
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-10-24 12:52:02 +0600

    service type updated

---
+++ src/data/ServiceTitles.ts
@@ -0,0 +1,14 @@
+export const serviceTitles: string[] = [
+  "House Cleaning",
+  "Lawn Service",
+  "Painting Services",
+  "Home Repairs",
+  "Electrical Services",
+  "Plumbing Services",
+  "Carpentry Services",
+  "Assembly & Installation",
+  "Kitchen Cabinet Installation",
+  "TV Mounting Services",
+  "Epoxy Flooring Services",
+  "Drywall Services",
+];
